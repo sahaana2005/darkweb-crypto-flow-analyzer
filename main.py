a78b067fac3da09ec1c41707e913e50ef22caf30
R.Sahaana <sahaanaraajkumar@gmail.com>
2026-03-13 12:40:55 +0530

    Wallet extraction module working

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from scraper.wallet_extractor import extract_wallets
-from blockchain.fetch_transactions import fetch_transactions
-from analysis.graph_builder import build_graph
-from visualization.dashboard import show_graph
+#from blockchain.fetch_transactions import fetch_transactions
+#from analysis.graph_builder import build_graph
+#from visualization.dashboard import show_graph
 
 text = """
-Vendor: Shadow Market
-Send payment to:
+Vendor listing
+Payment address:
 1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY
 """
 
 wallets = extract_wallets(text)
 
-transactions = fetch_transactions(wallets[0])
-
-graph = build_graph(transactions)
-
-show_graph(graph)
+print("Detected wallets:", wallets)
--- scraper/wallet_extractor.py
+++ scraper/wallet_extractor.py
@@ -1,2 +1,12 @@
+# Write a Python function called extract_wallets(text)
+# that extracts Bitcoin wallet addresses using regex
+# and returns them as a list
+import re
+
 def extract_wallets(text):
-    return ["wallet1", "wallet2"]
+
+    pattern = r'\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b'
+
+    wallets = re.findall(pattern, text)
+
+    return wallets 
